89fb7697218a9d48783b905edafbf25065831280
Osama-matter <osamamatter390@gmail.com>
2025-04-22 20:47:20 +0200

    Add files via upload

---
+++ matrix by rundom number.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include<stdio.h>
+using namespace std;
+int randowmnumber(int from, int to)
+{
+	int randoim = rand() % (to - from + 1) + from;
+	return randoim;
+}
+void Generatematrix(int maix[3][3])
+{
+	for (int i = 0;i < 3; i++)
+	{
+		for (int j = 0;j < 3;j++)
+		{
+			maix[i][j] =randowmnumber(1, 99);
+		}
+	}
+}
+void printmatrix(int maix[3][3])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0;j < 3;j++)
+		{
+			printf("%*0d  ", 2, maix[i][j]);
+		}
+		cout << endl;
+	}
+}
+int main()
+{
+	int maix[3][3];
+	Generatematrix(maix);
+	printmatrix(maix);
+	return 0;
+}
