1a213ffb86b8cce37150be582cb7187ba12f10f7
Osama-matter <osamamatter390@gmail.com>
2025-04-24 22:59:33 +0200

    Add files via upload

--- check identaty matrix.cpp.cpp
+++ check identaty matrix.cpp.cpp
@@ -8,23 +8,15 @@ int randowmnumber(int from, int to)
 	int randoim = rand() % (to - from + 1) + from;
 	return randoim;
 }
-void Generatematrix(int maix[3][3])
-{
-	for (int i = 0;i < 3; i++)
-	{
-		for (int j = 0;j < 3;j++)
-		{
-			maix[i][j] =randowmnumber(0,1);
-		}
-	}
-}
 bool checkifmatrixequlivant(int maix[3][3] )
 {
+	int check = 0 ;
 	for (int i = 0 ; i<3 ; i++)
 	{
 		for (int j=0 ;j<3 ; j++)
 		{
-			if (i == j && maix[i][j] != 1) // if i == j meaing i = 1 j= 1 (1,1 ) this is must be = 1 but in this condation i retrn false if (i ,j) !=0
+			check = maix[0][0] ;
+			if (i == j && maix[i][j] !=check) // if i == j meaing i = 1 j= 1 (1,1 ) this is must be = 1 but in this condation i retrn false if (i ,j) !=0
 			{
 				return false ;
 			}
@@ -40,11 +32,11 @@ void printresulats(int maix [3][3]  )
 {
 	if (checkifmatrixequlivant(maix))
 	{
-		cout << "matrix are identaty \n " ;
+		cout << "matrix is  scalar \n " ;
 	}
 	else 
 	{
-		cout << "matrix are Not identaty \n " ; 
+		cout << "matrix is Not  scalar \n " ; 
 	}
 }
 void printmatrix(int maix[3][3])
@@ -62,8 +54,14 @@ void printmatrix(int maix[3][3])
 int main()
 {
 	srand(time(0));
-	int maix[3][3];
-	Generatematrix(maix);
+	int maix[3][3] =
+	{
+		{9, 0, 0},
+		{0, 9, 0},
+		{0, 0, 9}
+	};
+	
+
 	printmatrix(maix);
 	printresulats(maix ) ;
 	return 0;
