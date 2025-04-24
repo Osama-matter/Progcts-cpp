ae9e30b2d1127e95e5b79b6a8284952e62f75b00
Osama-matter <osamamatter390@gmail.com>
2025-04-24 23:30:06 +0200

    Add files via upload

---
+++ check identaty matrix.cpp.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <string>
+#include<stdio.h>
+using namespace std;
+int randomnumber(int from, int to)
+{
+	int randoim = rand() % (to - from + 1) + from;
+	return randoim;
+}
+void Getnumber (int &number )
+{
+	 
+	do
+	{
+		cout << "Enter the number do you wont to check in matrix ? " ; 
+		cin >>number ;
+	} while (number <0); 
+}
+void Generatematrix(int maix[3][3])
+{
+	for (int i = 0;i < 3; i++)
+	{
+		for (int j = 0;j < 3;j++)
+		{
+			maix[i][j] =randomnumber(1, 9);
+		}
+	}
+}
+void printmatrix(int maix[3][3], int clo , int row   )
+{
+	
+	for (int i = 0; i < clo; i++)
+	{
+		for (int j = 0;j < row;j++)
+		{
+			printf("%*0d  ", 2, maix[i][j]);
+		}
+		cout << endl;
+	}
+	cout << "------------------------\n\n";
+}
+int clasumofmatrix(int maix[3][3] , int number )
+{
+	int sum = 0 ; 
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0;j < 3;j++)
+		{
+			if (number == maix[i][j])
+			{
+				sum ++ ;
+			}
+		}
+	}
+	return sum ;
+}
+
+int main()
+{
+	int maix[3][3];
+	int number = 0 ;
+	Generatematrix(maix);
+	printmatrix(maix , 3 , 3 ) ;
+	Getnumber(number) ;
+	clasumofmatrix(maix , number) ;
+	cout << "number ( " << number  <<" )count in matrix :" <<clasumofmatrix(maix, number ) ;
+	return 0;
+}
