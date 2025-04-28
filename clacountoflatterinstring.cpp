103836821ab1913a89becc68c0d0212cf954f585
Osama-matter <osamamatter390@gmail.com>
2025-04-28 12:20:30 +0300

    Add files via upload

--- clacountoflatterinstring.cpp
+++ clacountoflatterinstring.cpp
@@ -14,6 +14,7 @@ char Getchar()
     char  c1 = ' ';
     cout << "Enter char  ? \n";
     cin >> c1;
+    c1 = tolower(c1);
     return c1;
 
 }
@@ -23,15 +24,30 @@ void sumnumberodlower(string s1)
 }
 void clanumberofcapitallatterorSmall(string s1, char c1)
 {
+    int count2 = 0;
+    for (int i = 0; i < s1.length(); i++)
+    {
+      
+        if (s1[i] == c1)
+        {
+            count2++;
+        }
+    }
+    cout << "char '" << c1 << "' Count = " << count2 << endl ; 
+}
+void clanumberofcharintext(string s1, char c1)
+{
     int count = 0;
     for (int i = 0; i < s1.length(); i++)
     {
+        s1[i] = tolower(s1[i]);
         if (s1[i] == c1)
         {
             count++;
         }
     }
-    cout << "char ' << c1 << ' ]Count = " << count; 
+    char c1C = toupper(c1);
+    cout << "char '" << c1 << "' OR ' " <<  c1C << "' Count = " << count;
 }
 int main()
 {
@@ -39,6 +55,7 @@ int main()
     char c1 = Getchar();
     sumnumberodlower(s1);
     clanumberofcapitallatterorSmall(s1 , c1);
+    clanumberofcharintext(s1, c1);
     system("pause>0");
     return 0;
 }
