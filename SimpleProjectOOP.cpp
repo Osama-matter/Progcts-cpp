1dd374e666e27321ce9b37628fb297c3a29fdb5b
Osama-matter <osamamatter390@gmail.com>
2025-06-15 02:26:12 +0300

    Add files via upload

---
+++ SimpleProjectOOP.cpp
@@ -0,0 +1,149 @@
+#include<iostream> 
+using namespace std; 
+class Info
+{
+	string  _Id; 
+	string _FristName; 
+	string _LastName; 
+	string _FullName; 
+	string _Email; 
+	string _Phone;
+	string _Subject; 
+	string _Body;
+	string _Massage; 
+	void setFristName(string FristName)
+	{
+		_FristName = FristName;
+	}
+	void SetID(string  ID)
+	{
+		_Id = ID;
+	}
+	void setLastName(string LastName)
+	{
+		_LastName = LastName;
+	}
+	void SetFullName(string FullName)
+	{
+		_FullName = FullName;
+	}
+	void SetEmail(string Email)
+	{
+		_Email = Email;
+	}
+	void SetPhone(string Phone)
+	{
+		_Phone = Phone;
+	}
+	void SetSubject(string Subject)
+	{
+		_Subject = Subject; 
+	}
+	string GetSubject(string Subject)
+	{
+		SetSubject(Subject); 
+		return _Subject;
+	}
+	void SetBody(string Body)
+	{
+		_Body = Body; 
+	}
+	void SetMassage(string Massage)
+	{
+		_Massage = Massage; 
+	}
+	string GetBody(string Body)
+	{
+		SetBody(Body);
+		return _Body;
+	}
+	string GetMassage(string Massage)
+	{
+		SetMassage(Massage);
+		return _Massage;
+	}
+public :
+
+	Info(string Id  , string FristName , string LastName  , string FullName  , string Email , string Phone  )
+	{
+		_Id = Id; 
+		_FristName = FristName; 
+		_LastName = LastName; 
+		_FullName = FullName; 
+		_Email = Email; 
+		_Phone = Phone; 
+	}
+
+
+
+	string FristName(string FristName)
+	{
+		setFristName(FristName);
+		return _FristName; 
+	}
+
+	string  ID(string ID)
+	{
+		SetID(ID); 
+		return _Id; 
+	}
+
+	string LastName(string LastName)
+	{
+		setLastName(LastName); 
+		return _LastName; 
+
+	}
+
+	string FullName(string FullName)
+	{
+		SetFullName(FullName); 
+		return _FullName; 
+	}
+
+	string Email(string Email )
+	{
+		SetEmail(Email); 
+		return _Email; 
+	}
+
+	string Phone(string Phone )
+	{
+		SetPhone(Phone); 
+		return _Phone; 
+	}
+	void SendEmail(string Subject, string Body)
+	{
+		GetSubject(Subject );
+		GetBody(Body); 
+		cout << "The Follwoing email Has Send successful to email " << _Email  <<" \n"; 
+		cout << "Subiect :" << _Subject <<endl ;
+		cout << "Body :" << _Body << endl << endl ; 
+		
+	}
+	void SenSMS(string Massage)
+	{
+		GetMassage(Massage); 
+		cout << "The Follwoing SMS has Send successful to phone " << _Phone << "  \n"; 
+		cout << _Massage << endl; 
+	}
+	void Print()
+	{
+		cout << "\n--------------------------------------------\n"; 
+		cout << "ID          " << _Id <<endl ;
+		cout << "Frist Name  " << _FristName << endl; 
+		cout << "Last Name   " << _LastName << endl; 
+		cout << "Full Name   " << _FullName << endl; 
+		cout << "Enail       " << _Email << endl;
+		cout << "Phone       " << _Phone << endl; 
+		cout << "----------------------------------------------\n\n"; 
+	}
+
+};
+int main()
+{
+	Info Person1 ( "A101" , "Osama", "Matter ", "Osama Ahmed Matter ", "osamamatter390@gmail.com", "01273201805");
+	Person1.Print(); 
+	Person1.SendEmail("Hi", "How Are you"); 
+	Person1.SenSMS("YOu are great "); 
+}
